d361e293e38c219a0d02e1ec819700f1dde82f19
Arin <wbriody@gmail.com>
2025-09-11 21:03:07 -0400

    working?

---
+++ script/main.js
@@ -0,0 +1,118 @@
+const green = "🟩";
+const blue = "🟦";
+const red = "🟥";
+
+const guess_input = document.getElementById("guess-input");
+const commit = document.getElementById("commit");
+const copy = document.getElementById("copy");
+const code = document.getElementById("code");
+
+const keys = [];
+const guess = [];
+
+const body = document.getElementById("body");
+
+function cur_date() {
+	const today = new Date();
+	return `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`;
+}
+
+function getCookie(cname) {
+	let name = cname + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let ca = decodedCookie.split(';');
+	for(let i = 0; i <ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
+body.onload = function() {
+	const date_string = cur_date();
+
+	const last_play = localStorage.getItem("last");
+
+	if (last_play) {
+		const last = JSON.parse(last_play);
+
+		if (last.last_day == date_string) {
+			commit.style.visibility = hidden;
+			copy.style.visibility = visible;
+
+			code.innerHTML = last.last_guess;
+		}
+	}
+
+	let hash = 0;
+
+	for (let i = 0; i < date_string.length; i++) {
+		hash += date_string.charCodeAt(i);
+	}
+
+	hash *= 1103515245;
+	hash += 12345;
+	hash &= 2147483647;
+
+	for (let i = 0; i < 5; i++) {
+		keys.push(((hash & 31) % 27));
+		hash = hash >> 5;
+	}
+}
+
+guess_input.onkeypress = function(e) {
+	char_regex = RegExp("[A-Za-z]");
+
+	if(!char_regex.test(e.key)) {
+		e.preventDefault();
+	}
+}
+
+commit.onclick = function() {
+	var guess_string = guess_input.value;
+
+	if (guess_string.length < 5) { return; }
+	guess_string = guess_string.toLowerCase();
+
+	for (let i = 0; i < guess_string.length; i++) {
+		let cur = guess_string.charCodeAt(i) - 97;
+
+		if ((keys[i] - cur) == 0) {
+			guess.push(green);
+		}
+		else if (Math.abs(keys[i] - cur) <= 3) {
+			guess.push(blue);
+		}
+		else {
+			guess.push(red);
+		}
+	}
+
+	const guess_final = `${guess[0]}${guess[1]}${guess[2]}${guess[3]}${guess[4]}`;
+
+	const last = {
+		last_day: cur_date(),
+		last_guess: guess_final
+	};
+	
+	localStorage.setItem("last", JSON.stringify(last));
+
+	code.innerHTML = guess_final;
+
+	copy.style.visibility = "visible";
+	commit.style.visibility = "hidden";
+}
+
+copy.onclick = function() {
+	const emoji = code.innerHTML;
+	const moments = Math.floor(Math.random() * 11);
+
+	const score = `${emoji} | Slabdle \n${cur_date()}\n\nGot in: ${moments} moments \n\nhttps://arronbscotch.github.io/slabdle/`;
+
+	navigator.clipboard.writeText(score);
+}
